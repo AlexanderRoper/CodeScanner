052162069400dd9e68632afb2b0181d5e1ec1a11
AlexanderRoper <40900372+AlexanderRoper@users.noreply.github.com>
2023-01-18 09:45:14 -0500

    Update ScannerViewController.swift
    Go for it

--- Sources/CodeScanner/ScannerViewController.swift
+++ Sources/CodeScanner/ScannerViewController.swift
@@ -302,8 +302,8 @@ extension CodeScannerView {
             NSLayoutConstraint.activate([
                 imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
                 imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                imageView.widthAnchor.constraint(equalToConstant: 325),
-                imageView.heightAnchor.constraint(equalToConstant: 325),
+                imageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+                imageView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.8),
             ])
         }
 
